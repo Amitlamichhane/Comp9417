92d3b97dab00af745748e583f10cb731ff17c00a
Johann <lamichhaneamit7@gmail.com>
2018-05-31 14:52:55 +1000

    Read files to numpy array

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import arff, numpy as np
 
-#testing python files with add and delete 
+
+#read arf files and convert it into numpy 
+def read_arf_data (file_name):
+    dataset = arff.load(open(file_name, 'r'))
+    numpy_data = np.array(dataset['data'])#read like a numpy array 
+    return numpy_data
+    
+
+
+if __name__ == '__main__':
+    data = read_arf_data('autoMpg.arff')
+    print(data)
